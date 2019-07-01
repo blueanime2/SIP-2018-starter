3f371e4e3968a0e55d4c3425931e281e7ab50d09
blueanime2 <simoneachucarro@gmail.com>
2019-07-01 14:51:09 -0700

    did a thing uwu

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -14,7 +14,46 @@ tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below! 
+tweet_text = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet['text']
+    x = tweet['text']
+    tweet_text.append(x)
+# print(tweetstring)
 
 # Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+polarity = []
+subjectivity = []
+
+# print(tweet_text)
+for tweet in tweet_text:
+    tb = TextBlob(tweet)
+    subjectivity.append(tb.subjectivity)
+    polarity.append(tb.polarity)
+tweetBlob = TextBlob(tweetstring)
+word_dict = {}
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+    print(word_dict)
+
+
+
+# print(subjectivity)
+# print(polarity)
+
+# y = sum(polarity)/len(polarity)
+# w = sum(subjectivity)/len(subjectivity)
+# print(y)
+# print(w)
+
+# import matplotlib.pyplot as plt
+
+# plt.hist(polarity, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.hist(subjectivity, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.xlabel('Values')
+# plt.ylabel('Number of Items')
+# plt.title('Histogram of Numbers')
+# plt.axis([-1.1, 1.1, 0, 100])
+# plt.grid(True)
+# plt.show()
